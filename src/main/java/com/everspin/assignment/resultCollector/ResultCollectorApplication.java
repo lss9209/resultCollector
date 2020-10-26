56241c1052333b4cb06bf82c980a0ab4bf065431
lss92 <lss9209@naver.com>
2020-10-26 18:59:10 +0900

    더미넣는것까지 순조로움

--- src/main/java/com/everspin/assignment/resultCollector/ResultCollectorApplication.java
+++ src/main/java/com/everspin/assignment/resultCollector/ResultCollectorApplication.java
@@ -1,13 +1,17 @@
 package com.everspin.assignment.resultCollector;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ResultCollectorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ResultCollectorApplication.class, args);
+
+		SpringApplication app = new SpringApplication(ResultCollectorApplication.class);
+		app.setWebApplicationType(WebApplicationType.NONE);
+		app.run(args);
 	}
 
 }
